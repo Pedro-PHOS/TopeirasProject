03b81b3949e0f705565f8d09c0e6f810e52529f3
Pedro Henrique Oliveira Silva <84513218+Pedro-PHOS@users.noreply.github.com>
2024-06-19 20:08:34 -0300

    up

--- GetAnimals.js
+++ GetAnimals.js
@@ -3,6 +3,8 @@ const AnimalList = document.getElementById("AnimalList");
 // URL da API
 const apiUrl = 'https://freetestapi.com/api/v1/animals';
 
+
+
 // Fazer uma requisição GET
 fetch(apiUrl)
   .then(response => {
@@ -12,30 +14,70 @@ fetch(apiUrl)
     }
     return response.json(); // Converter a resposta para JSON
   })
-  
-    .then(data => {
-        console.log(data);
+  .then(data => {
+    console.log(data)
 
-        data.forEach(animal => {
+    const imagesAnimal = [
+      { id: 1, image: "Leão" },
+      { id: 2, image: "Elefante" },
+      { id: 3, image: "Tigre" }
+    ];
+    
+    data.forEach(animal => {
+      let encontrado = false;
+    
+      imagesAnimal.forEach(animalImagem => {
+        if (animalImagem.id === animal.id) {
+          encontrado = true;
+        }
+      });
+    
+      if (encontrado) {
+        console.log("Verdadeiro");
+      } else {
+        console.log("Falso");
+        console.log(animal.name);
+      }
 
-            const AnimalCard = document.createElement('div');
-            AnimalCard.classList.add('CardAnimal');
-            AnimalCard.setAttribute('id', animal.name);
+        const AnimalCard = document.createElement('div');
+        AnimalCard.classList.add('CardAnimal');
+        AnimalCard.setAttribute('id', animal.name);
 
-            AnimalCard.innerHTML = `
-                <div class="AnimalPhoto"> ${animal.image} </div>
-                
-                <div class="AnimalDescription">
-                    <h1> Nome: ${animal.name}</h1>
-                    <p>Especie: ${animal.species}</p>
-                    <p>Dieta: ${animal.diet}</p>
-                    <p>Descrição: ${animal.description} </p>
-                </div>
+        AnimalCard.innerHTML = `
+            <div class="AnimalPhoto"><img src="${animal.image}"></div>
+            
+            <div class="AnimalDescription">
+                <h1> Nome: ${animal.name}</h1>
+                <p>Especie: ${animal.species}</p>
+                <p>Dieta: ${animal.diet}</p>
+                <p>Descrição: ${animal.description} </p>
+            </div>
+        `;
 
-            `;
+        AnimalList.appendChild(AnimalCard);
+      });
 
-            AnimalList.appendChild(AnimalCard);
+  });
 
-        });
+function SearchAnimal() {
 
-    });
+  // Obtém o valor de entrada do usuário e converte para maiúsculas
+  const searchInput = document.getElementById("SearchInput");
+  const filter = searchInput.value.toUpperCase();
+
+  // Obtém todos os elementos com a classe 'CardAnimal'
+  const items = document.getElementsByClassName("CardAnimal");
+
+  // Itera sobre todos os itens
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i];
+    const idValue = item.id.toUpperCase(); // Obtém o ID do elemento e converte para maiúsculas
+
+    // Verifica se o filtro está presente no ID do item
+    if (idValue.indexOf(filter) > -1) {
+      item.style.display = ""; // Exibe o item se corresponder ao filtro
+    } else {
+      item.style.display = "none"; // Oculta o item se não corresponder ao filtro
+    }
+  }
+}
---
+++ img.js
@@ -0,0 +1,7 @@
+const imagesAnimal = [
+    {id: 1, image: "Lion"},
+
+    {id: 2, image: "Elephant"},
+
+    {id: 3, image: "Tiger"}
+];
