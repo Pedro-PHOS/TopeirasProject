3e16e68591fc02b525ffe0d0ae4ce5c58132111e
Pedro Henrique Oliveira Silva <84513218+Pedro-PHOS@users.noreply.github.com>
2024-06-24 09:04:34 -0300

    up

--- GetAnimals.js
+++ GetAnimals.js
@@ -1,7 +1,13 @@
-const AnimalList = document.getElementById("AnimalList");
-
-// URL da API
+const animalList = document.getElementById("animalList");
+const showPage = document.getElementById('Pagina');
 const apiUrl = 'https://freetestapi.com/api/v1/animals';
+const searchBar = document.getElementById('searchBar');
+
+
+let page = 1;
+let animalsData = [];
+
+showPage.innerHTML = page;
 
 // Fazer uma requisição GET
 fetch(apiUrl)
@@ -12,48 +18,84 @@ fetch(apiUrl)
     }
     return response.json(); // Converter a resposta para JSON
   })
-
   .then(data => {
+    animalsData = data;
+    let Filter = false;
+    loadPage(page, Filter); // Carregar a primeira página
+  })
+  .catch(error => {
+    console.error('Erro ao carregar os dados:', error);
+  });
 
-    data.forEach(animal => {
-        const AnimalCard = document.createElement('div');
-        AnimalCard.classList.add('CardAnimal');
-        AnimalCard.setAttribute('id', animal.name);
+function cardgenerator(i, data){
+  const AnimalCard = document.createElement('div');
+  AnimalCard.classList.add('CardAnimal');
+  AnimalCard.setAttribute('id', data[i].name);
 
-        AnimalCard.innerHTML = `
-          <div class="AnimalPhoto"><img src="${animal.image}"></div>
-          
-          <div class="AnimalDescription">
-            <h1> Nome: ${animal.name}</h1>
-            <p>Especie: ${animal.species}</p>
-            <p>Dieta: ${animal.diet}</p>
-            <p>Descrição: ${animal.description} </p>
-          </div>`;
+  AnimalCard.innerHTML = `
+    <div class="AnimalPhoto"><img src="${data[i].image}"></div>
+    <div class="AnimalDescription">
+      <h1>Nome: ${data[i].name}</h1>
+      <p>Especie: ${data[i].species}</p>
+      <p>Dieta: ${data[i].diet}</p>
+      <p>Descrição: ${data[i].description}</p>
+    </div>`;
 
-        AnimalList.appendChild(AnimalCard);
-            
-      })
-    });
+  animalList.appendChild(AnimalCard);
+}
+
+function loadPage(page, Filter) {
+  if(Filter == true){
+    let x = 50
+    myAss(x)
+  }
+  else{let x = 10
+    myAss(x)
+  }
+
+  function myAss(x){
+    animalList.innerHTML = ''; // Limpar a lista atual
+    const start = (page - 1) * 10;
+    const end = start + x;
+    for (let i = start; i < end && i < animalsData.length; i++) {
+      cardgenerator(i, animalsData);
+    }
+  }
+}
+
+function Next() {
+  if (page < Math.ceil(animalsData.length / 10)) {
+    page += 1;
+    showPage.innerHTML = page;
+    loadPage(page);
+  }
+}
+
+function Preview() {
+  if (page > 1) {
+    page -= 1;
+    showPage.innerHTML = page;
+    loadPage(page);
+  }
+}
 
 function SearchAnimal() {
-
-  // Obtém o valor de entrada do usuário e converte para maiúsculas
   const searchInput = document.getElementById("SearchInput");
   const filter = searchInput.value.toUpperCase();
-
-  // Obtém todos os elementos com a classe 'CardAnimal'
   const items = document.getElementsByClassName("CardAnimal");
 
-  // Itera sobre todos os itens
+  let Filter = true;
+
+  loadPage(1, Filter);
+
   for (let i = 0; i < items.length; i++) {
     const item = items[i];
-    const idValue = item.id.toUpperCase(); // Obtém o ID do elemento e converte para maiúsculas
+    const idValue = item.id.toUpperCase();
 
-    // Verifica se o filtro está presente no ID do item
     if (idValue.indexOf(filter) > -1) {
-      item.style.display = ""; // Exibe o item se corresponder ao filtro
+      item.style.display = "";
     } else {
-      item.style.display = "none"; // Oculta o item se não corresponder ao filtro
+      item.style.display = "none";
     }
   }
 }
---
+++ images/GetAnimalsOld.js
@@ -0,0 +1,58 @@
+const AnimalList = document.getElementById("AnimalList");
+
+// URL da API
+const apiUrl = 'https://freetestapi.com/api/v1/animals';
+
+// Fazer uma requisição GET
+fetch(apiUrl)
+  .then(response => {
+    // Verificar se a resposta é bem-sucedida
+    if (!response.ok) {
+      throw new Error('Erro na rede: ' + response.statusText);
+    }
+    return response.json(); // Converter a resposta para JSON
+  })
+  .then(data => {
+    data.forEach(animal => {
+      
+        const AnimalCard = document.createElement('div');
+        AnimalCard.classList.add('CardAnimal');
+        AnimalCard.setAttribute('id', animal.name);
+
+        AnimalCard.innerHTML = `
+          <div class="AnimalPhoto"><img src="${animal.image}"></div>
+          
+          <div class="AnimalDescription">
+            <h1> Nome: ${animal.name}</h1>
+            <p>Especie: ${animal.species}</p>
+            <p>Dieta: ${animal.diet}</p>
+            <p>Descrição: ${animal.description} </p>
+          </div>`;
+
+        AnimalList.appendChild(AnimalCard);
+            
+      })
+    });
+
+function SearchAnimal() {
+
+  // Obtém o valor de entrada do usuário e converte para maiúsculas
+  const searchInput = document.getElementById("SearchInput");
+  const filter = searchInput.value.toUpperCase();
+
+  // Obtém todos os elementos com a classe 'CardAnimal'
+  const items = document.getElementsByClassName("CardAnimal");
+
+  // Itera sobre todos os itens
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i];
+    const idValue = item.id.toUpperCase(); // Obtém o ID do elemento e converte para maiúsculas
+
+    // Verifica se o filtro está presente no ID do item
+    if (idValue.indexOf(filter) > -1) {
+      item.style.display = ""; // Exibe o item se corresponder ao filtro
+    } else {
+      item.style.display = "none"; // Oculta o item se não corresponder ao filtro
+    }
+  }
+}
